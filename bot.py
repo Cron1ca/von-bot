cbf8dbdea797e342f69496d0802bb3863413a348
Cron1ca <85397788+Cron1ca@users.noreply.github.com>
2021-08-21 17:33:30 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
-from discord.ext import commands
+from discord import client
+from discord.ext import commands, tasks
 import json
 import os
+import random
 
 
 
@@ -10,12 +12,19 @@ import os
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="v.", intents = intents)
-
+bot.time = random.randint(10, 15)
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"yo bitch"))
+
+
+@tasks.loop(minutes=bot.time)
+async def play_m():
+    bot.time = random.randint(10, 15)
+    channel = bot.get_channel(878423065149726764)
+    await channel.connect()
 
 
 
